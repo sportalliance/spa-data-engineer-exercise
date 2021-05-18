45efb582b5afbd07750ff9a974b9a9bf0eef99f4
Florian Müller <florian.mueller@sportalliance.com>
2021-05-18 10:38:26 +0200

    fix: more plausible data

--- src/playground/generation.py
+++ src/playground/generation.py
@@ -6,7 +6,8 @@ from pyspark.sql import SQLContext, DataFrame, SparkSession
 
 NUM_TENANTS = 100
 NUM_IDS = 100_000
-START_YEAR = 2021
+START_YEAR = 2020
+NUM_MONTH = 8
 NUM_COLS = 50
 SEED = 42
 NUM_PARTITIONS = 10
@@ -19,26 +20,26 @@ def gen_table_data(spark: SQLContext) -> DataFrame:
         [tuple([i]) for i in range(1, NUM_IDS + 1)], schema=["id"]
     )
     ids_df = ids_df.withColumn("modified_date", F.lit(datetime(START_YEAR, 1, 1, 15)))
+    records_per_month = NUM_IDS / NUM_MONTH
     ids_df = ids_df.withColumn(
         "modified_date",
-        F.expr(f"date_add(add_months(modified_date, floor({NUM_IDS} / 12)), id % 27)"),
+        F.expr(
+            f"date_add(add_months(modified_date, floor(id / {records_per_month})), id % 27)"
+        ),
     )
     for i in range(NUM_COLS):
         col_type = i % 3
         if col_type == 0:
             ids_df = ids_df.withColumn(
-                f"col_{i}", F.floor(F.randn(SEED) * 10 + i).cast("int")
+                f"col_{i}", F.floor(F.randn(SEED + i) * 10 + i).cast("int")
             )
         elif col_type == 1:
-            ids_df = ids_df.withColumn(f"col_{i}", F.rand(SEED))
+            ids_df = ids_df.withColumn(f"col_{i}", F.rand(SEED + i))
         else:
             ids_df = ids_df.withColumn(
                 f"col_{i}",
-                F.format_string(
-                    "STATUS",
-                    F.when(F.rand(SEED) < 0.5, F.lit(f"VALID_{i}")).otherwise(
-                        F.lit(f"INVALID_{i}")
-                    ),
+                F.when(F.rand(SEED + i) < 0.5, F.lit(f"VALID_{i}")).otherwise(
+                    F.lit(f"INVALID_{i}")
                 ),
             )
 
